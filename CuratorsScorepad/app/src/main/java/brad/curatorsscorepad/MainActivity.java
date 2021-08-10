b3305dc66cc2a060d1bfbb197f6bd222ecaa4ebc
bradkisaacs <bradkisaacs@gmail.com>
2021-08-10 15:44:30 -0600

    Added second Activity that will get players names

--- CuratorsScorepad/app/src/main/java/brad/curatorsscorepad/MainActivity.java
+++ CuratorsScorepad/app/src/main/java/brad/curatorsscorepad/MainActivity.java
@@ -2,13 +2,24 @@ package brad.curatorsscorepad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button score, history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        score = findViewById(R.id.btn_score_game);
+        history = findViewById(R.id.btn_history);
+
+        score.setOnClickListener(v -> {
+            startActivity(new Intent(this, Players.class));
+        });
     }
 }
