e10716d0de8855e198b75a2ef1ca9763235a99d0
bradkisaacs <bradkisaacs@gmail.com>
2021-08-11 10:14:53 -0600

    added an option to clear game history

--- CuratorsScorepad/app/src/main/java/brad/curatorsscorepad/History.java
+++ CuratorsScorepad/app/src/main/java/brad/curatorsscorepad/History.java
@@ -1,8 +1,12 @@
 package brad.curatorsscorepad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class History extends AppCompatActivity {
@@ -19,6 +23,29 @@ public class History extends AppCompatActivity {
         display = findViewById(R.id.tv_history);
         display.setText("");
         load();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.history_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_clear_history:
+                clearShared();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void clearShared() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        sharedPreferences.edit().clear().commit();
+        display.setText("");
     }
 
     public void load(){
